e033ccd7054f25f8bf0f6d39d461efacc954eafc
Bruno Carraro <obrunocarraro@gmail.com>
2025-04-13 14:58:40 -0300

    import axios dependencie

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Cria uma instância do axios com a URL base da API
 const apiClient = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'https://localhost:7258/api',
+  baseURL: process.env.REACT_APP_API_URL || 'https://sistemazoolgicoapi.azurewebsites.net/api',
   headers: {
     'Content-Type': 'application/json'
   }
--- src/services/cuidadoService.js
+++ src/services/cuidadoService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://localhost:7258/api';
+const API_URL = 'https://sistemazoolgicoapi.azurewebsites.net/api';
 
 export const getCuidados = async () => {
   try {
